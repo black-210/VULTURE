42edaad51b1581671f19a097ad9d3aaf848c7fdf
D3vil0p3r <vozaanthony@gmail.com>
2026-09-08 22:52:22 +0200

    Refactor imports and update __all__ exports - fix the invocation of gui and cli module
    Fix the invocation of gui and cli module when the tool is installed through PEP517 python installer

--- src/vulture/core/__init__.py
+++ src/vulture/core/__init__.py
@@ -1,15 +1,13 @@
 """VULTURE Core Engine - Framework Registry, Dependency Injection, Configuration."""
-
 from .framework_registry import FrameworkRegistry
-from .dependency_injection import DependencyInjector
+from .dependency_injection import DependencyInjection
+from .dependency_injection import DependencyInjection as DependencyInjector
 from .config_manager import ConfigurationManager
+from .config import ConfigManager
+from .permission_manager import PermissionManager
 from .plugin_system import PluginSystem
 from .security_policy import SecurityPolicy
 
-__all__ = [
-    'FrameworkRegistry',
-    'DependencyInjector',
-    'ConfigurationManager',
-    'PluginSystem',
-    'SecurityPolicy',
-]
+__all__ = ['FrameworkRegistry', 'DependencyInjection', 'DependencyInjector',
+           'ConfigurationManager', 'ConfigManager', 'PermissionManager',
+           'PluginSystem', 'SecurityPolicy']
